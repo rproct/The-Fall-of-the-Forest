9fa0992cac33a640a54621def2791fddc2682e7e
Ryan Proctor <49620154+rproct@users.noreply.github.com>
2020-09-30 15:43:13 -0700

    Fixing master

---
+++ Fall of the Forest/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float height;
+    public float jumpForce;
+    private Rigidbody rb;
+    private bool grounded;
+    private Vector3 jump;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0f, height, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float translation = Input.GetAxis("Horizontal");
+        if (CanJump()) 
+            TriggerJump();
+
+        translation *= Time.deltaTime;
+        
+        transform.Translate(0, 0, translation);
+    }
+
+    private void OnCollisionStay()
+    {
+        grounded = true;
+    }
+
+    private bool CanJump()
+    {
+        return Input.GetButton("Jump") && grounded;
+    }
+
+    private void TriggerJump()
+    {
+        rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+        grounded = false;
+    }
+}
