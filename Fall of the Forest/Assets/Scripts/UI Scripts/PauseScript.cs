91481caceb01ba9249c5a9f973afe17a0400e696
Ryan Proctor <49620154+rproct@users.noreply.github.com>
2020-10-23 12:51:38 -0700

    DRYed up menu system

--- Fall of the Forest/Assets/Scripts/UI Scripts/PauseScript.cs
+++ Fall of the Forest/Assets/Scripts/UI Scripts/PauseScript.cs
@@ -2,87 +2,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UI_Scripts;
 using UnityEngine;
 
-public class PauseScript : MonoBehaviour
+namespace UI_Scripts
 {
-    public TextMeshProUGUI[] tmps;
-    private int selection;
-    private static readonly Color32 white = new Color32(255, 255, 255, 255);
-    private static readonly Color32 yellow = new Color32(201, 171, 0, 255);
-    private bool buttonUse;
-    private void OnEnable()
+    public class PauseScript : Menu
     {
-        foreach (TextMeshProUGUI t in tmps)
+        public TextMeshProUGUI[] temp;
+
+        private void OnEnable()
         {
-            t.faceColor = white;
+            InitializeMenu(temp);
         }
 
-        selection = 0;
-        tmps[selection].faceColor = yellow;
-        buttonUse = false;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetButtonDown("Cancel"))
-            Unpause();
-        
-        if(Input.GetButtonDown("Submit"))
-            executeSelection();
-        
-        if (Input.GetAxisRaw("Vertical") < 0)
+        // Update is called once per frame
+        void Update()
         {
-            if (buttonUse == false)
+            if (Input.GetButtonDown("Cancel"))
+                Unpause();
+
+            if (Input.anyKey)
+                getInput();
+            else
+                buttonUse = false;
+        }
+
+        protected override void executeSelection()
+        {
+            switch (selection)
             {
-                changeSelection(1);
-                buttonUse = true;
+                case 0:
+                    Unpause();
+                    break;
+                case 1:
+                    Unpause();
+                    UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager
+                        .GetActiveScene().name);
+                    break;
+                case 2:
+                    Unpause();
+                    UnityEngine.SceneManagement.SceneManager.LoadScene("Title Scene");
+                    break;
             }
         }
-        else if (Input.GetAxisRaw("Vertical") > 0)
-        {
-            if (buttonUse == false)
-            {
-                changeSelection(-1);
-                buttonUse = true;
-            }
-        }
-        else
-            buttonUse = false;
-    }
-    
-    private void changeSelection(int offset)
-    {
-        tmps[selection].faceColor = white;
-        selection = (selection + offset) % tmps.Length;
-        if (selection < 0)
-            selection = tmps.Length - 1;
-        tmps[selection].faceColor = yellow;
-    }
 
-    private void executeSelection()
-    {
-        switch (selection)
+        private void Unpause()
         {
-            case 0:
-                Unpause();
-                break;
-            case 1:
-                Unpause();
-                UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager
-                    .GetActiveScene().name);
-                break;
-            case 2:
-                Unpause();
-                UnityEngine.SceneManagement.SceneManager.LoadScene("Title Scene");
-                break;
+            Time.timeScale = 1;
+            gameObject.SetActive(false);
         }
-    }
-
-    private void Unpause()
-    {
-        Time.timeScale = 1;
-        gameObject.SetActive(false);
     }
 }
